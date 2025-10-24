a7dc8a2701a7cb3ebb8c2866aeb70f7e8c3478d7
StefanNazarov76 <stefannazarov76@gmail.com>
2025-10-24 12:10:33 +0300

    add: swap_case.py

---
+++ Python/swap_case.py
@@ -0,0 +1,16 @@
+def swap_case(s):
+    new_string = ''
+
+    for i in s:
+        if i.islower():
+            new_string += i.upper()
+        else:
+            new_string += i.lower()
+
+    return new_string
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
