b960fb59da74d9c49b6474ddb03e991893261bd9
StefanNazarov76 <stefannazarov76@gmail.com>
2025-11-09 11:03:51 +0200

    add: the_love_letter_mystery.py

---
+++ Algorithms/the_love_letter_mystery.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theLoveLetterMystery' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def theLoveLetterMystery(s):
+    # Write your code here
+    moves = 0
+    n = len(s)
+    for i in range(n // 2):
+        moves += abs(ord(s[i]) - ord(s[n - i - 1]))
+    return moves
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
