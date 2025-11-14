e15ddbce81b144051a547b26dd028fd4a1ae3308
StefanNazarov76 <stefannazarov76@gmail.com>
2025-11-14 12:05:35 +0200

    add: day_of_the_programmer.py

---
+++ Algorithms/day_of_the_programmer.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+def dayOfProgrammer(year):
+    # Write your code here
+    if year == 1918:
+        return f'26.09.{year}'
+
+    if year < 1918:
+        return f'12.09.{year}' if year % 4 == 0 else f'13.09.{year}'
+
+    return f'12.09.{year}' if (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)) else f'13.09.{year}'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
