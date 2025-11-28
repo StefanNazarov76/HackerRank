368c2260a52714bc82204c1766981ae8cb3c062e
StefanNazarov76 <stefannazarov76@gmail.com>
2025-11-28 12:39:35 +0200

    add: running_time_of_algorithms.py

---
+++ Algorithms/running_time_of_algorithms.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'runningTime' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def runningTime(arr):
+    # Write your code here
+    shifts = 0
+    for i in range(1, len(arr)):
+        value = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > value:
+            arr[j + 1] = arr[j]
+            j -= 1
+            shifts += 1
+
+        arr[j + 1] = value
+
+    return shifts
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
