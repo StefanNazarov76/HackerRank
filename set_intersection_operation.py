25221255afb3edf94af25fa62e9e7bdd7ede283f
StefanNazarov76 <stefannazarov76@gmail.com>
2025-12-02 20:20:20 +0200

    add: set_intersection_operation.py

---
+++ set_intersection_operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+_ = int(input())
+a = set(input().split())
+
+_ = int(input())
+b = set(input().split())
+
+print(len(a.intersection(b)))
